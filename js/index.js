4af4c22bfb24f4bd21e8f5ea0e05c9098e1f1a15
Walter Smith <walter.c.smith01@gmail.com>
2021-01-19 10:17:17 -0600

    Completed JavaScript function that returns true if one of two arguments are 100 or if the sum of the two arguments are 100

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 "use strict"
+
+/*Write a Javascript program to check two numbers & returns true if one of the numbers are 100 or if the sum of the two numbers is 100*/
+
+function is100OrSumOf100(num1,num2) {
+    if (  num1 === 100 ||  num2 === 100 ){
+        return true;
+    }else   if ( num1 + num2 === 100){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+console.log(is100OrSumOf100("100", 50));
+console.log(is100OrSumOf100(0, 100));
+console.log(is100OrSumOf100(100, 0));
+console.log(is100OrSumOf100(50, 50));
+console.log(is100OrSumOf100(49.5, 49.5));
+
+
+
+/*Write a JavaScript program to get the extension of a filename*/
+
